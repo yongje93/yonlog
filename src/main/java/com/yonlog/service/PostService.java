dff7450865ca12ac4a477268cf4d31e5095f0714
Yongje Shin <yongje1211@gmail.com>
2023-07-27 23:02:23 +0900

    :sparkles: 예외 처리

--- src/main/java/com/yonlog/service/PostService.java
+++ src/main/java/com/yonlog/service/PostService.java
@@ -2,6 +2,7 @@ package com.yonlog.service;
 
 import com.yonlog.domain.Post;
 import com.yonlog.domain.PostEditor;
+import com.yonlog.exception.PostNotFound;
 import com.yonlog.repository.PostRepository;
 import com.yonlog.request.PostCreate;
 import com.yonlog.request.PostEdit;
@@ -33,7 +34,7 @@ public class PostService {
 
     public PostResponse get(Long id) {
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글입니다."));
+                .orElseThrow(PostNotFound::new);
 
         return PostResponse.builder()
                 .id(post.getId())
@@ -51,7 +52,7 @@ public class PostService {
     @Transactional
     public PostResponse edit(Long id, PostEdit postEdit) {
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글입니다."));
+                .orElseThrow(PostNotFound::new);
 
         PostEditor.PostEditorBuilder editorBuilder = post.toEditor();
 
@@ -67,7 +68,7 @@ public class PostService {
 
     public void delete(Long id) {
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글입니다."));
+                .orElseThrow(PostNotFound::new);
 
         postRepository.delete(post);
     }
