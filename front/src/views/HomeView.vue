1e2a259c08e0ad0a90adb8e1661d156ddbe09fc3
Yongje Shin <yongje1211@gmail.com>
2023-08-03 23:10:15 +0900

    :sparkles: 글 리스트 화면 생성

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,8 +1,48 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+const posts = ref([]);
+
+axios.get("/api/posts?page=1&size=5").then((response) => {
+  response.data.forEach((r: any) => {
+    posts.value.push(r);
+  });
+});
+
+const moveToRead = () => {
+  router.push({name: "read"})
+};
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="post in posts" :key="post.id" @click="moveToRead()">
+      <div>
+        <router-link :to="{ name: 'read', params: { postId: post.id } }">
+          {{ post.title }}
+        </router-link>
+      </div>
+
+      <div>
+        {{ post.content }}
+      </div>
+
+    </li>
+  </ul>
 </template>
+
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+
+  li {
+    margin-bottom: 2rem;
+  }
+}
+</style>
