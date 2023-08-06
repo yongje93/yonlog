f2fc7500e259496c267e0cf7b2d14d8fa29a60bd
Yongje Shin <yongje1211@gmail.com>
2023-08-06 22:30:31 +0900

    :sparkles: 화면 디자인 수정

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
 
@@ -8,6 +6,8 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+
+import "normalize.css";
 
 import "bootstrap/dist/css/bootstrap-utilities.css";
 
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -13,36 +13,69 @@ axios.get("/api/posts?page=1&size=5").then((response) => {
   });
 });
 
-const moveToRead = () => {
-  router.push({name: "read"})
-};
-
 </script>
 
 <template>
   <ul>
-    <li v-for="post in posts" :key="post.id" @click="moveToRead()">
-      <div>
+    <li v-for="post in posts" :key="post.id">
+      <div class="title">
         <router-link :to="{ name: 'read', params: { postId: post.id } }">
           {{ post.title }}
         </router-link>
       </div>
 
-      <div>
+      <div class="content">
         {{ post.content }}
       </div>
 
+      <div class="sub d-flex">
+        <div class="category">개발</div>
+        <div class="regDate">2023-08-06</div>
+      </div>
     </li>
   </ul>
 </template>
 
-<style>
+<style scoped lang="scss">
 ul {
   list-style: none;
   padding: 0;
 
   li {
     margin-bottom: 2rem;
+
+    .title {
+      a {
+        font-size: 1.1rem;
+        color: #383838;
+        text-decoration: none;
+      }
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    .content {
+      font-size: 0.85rem;
+      margin-top: 8px;
+      line-height: 1.4;
+      color: #7e7e7e;
+    }
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+    .sub {
+      margin-top: 8px;
+      font-size: 0.78rem;
+
+      .regDate {
+        margin-left: 10px;
+        color: #6b6b6b;
+      }
+    }
   }
 }
 </style>
