8114e4b37b147427a1a9909149b232c6a52b43b2
Yongje Shin <yongje1211@gmail.com>
2023-08-10 22:58:44 +0900

    :sparkles: 데이터베이스를 통한 토큰 검증

--- src/main/java/com/yonlog/config/AuthResolver.java
+++ src/main/java/com/yonlog/config/AuthResolver.java
@@ -1,14 +1,20 @@
 package com.yonlog.config;
 
 import com.yonlog.config.data.UserSession;
+import com.yonlog.domain.Session;
 import com.yonlog.exception.Unauthorized;
+import com.yonlog.repository.SessionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    private final SessionRepository sessionRepository;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -22,9 +28,9 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
             throw new Unauthorized();
         }
 
-        // 데이터베이스 사용자 확인 작업
-        // ...
+        Session session = sessionRepository.findByAccessToken(accessToken)
+                .orElseThrow(Unauthorized::new);
 
-        return new UserSession(1L);
+        return new UserSession(session.getUser().getId());
     }
 }
--- src/main/java/com/yonlog/repository/SessionRepository.java
+++ src/main/java/com/yonlog/repository/SessionRepository.java
@@ -3,6 +3,9 @@ package com.yonlog.repository;
 import com.yonlog.domain.Session;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SessionRepository extends CrudRepository<Session, Long> {
 
+    Optional<Session> findByAccessToken(String accessToken);
 }
