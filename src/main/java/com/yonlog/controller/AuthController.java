11d4d2bbcbbbe0b4f634c815b70a434a122f106d
Yongje Shin <yongje1211@gmail.com>
2023-08-11 22:28:02 +0900

    :sparkles: 쿠키를 통한 인증 및 검증

--- src/main/java/com/yonlog/controller/AuthController.java
+++ src/main/java/com/yonlog/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.yonlog.controller;
 
 import com.yonlog.request.Login;
-import com.yonlog.response.SessionResponse;
 import com.yonlog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -19,8 +22,21 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody @Valid Login login) {
+    public ResponseEntity<Object> login(@RequestBody @Valid Login login) {
         String accessToken = authService.signin(login);
-        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict") // 서로 다른 도메인간의 쿠키 전송에 대한 보안을 설정
+                .build();
+
+        log.info(">>>> cookie={}", cookie);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
