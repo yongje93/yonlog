48c65200d058152983ae71e929b420e9f8dd9809
Yongje Shin <yongje1211@gmail.com>
2023-08-24 23:32:41 +0900

    :sparkles: UserRepository update

--- src/main/java/com/yonlog/repository/UserRepository.java
+++ src/main/java/com/yonlog/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.yonlog.repository;
 
 import com.yonlog.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
 
